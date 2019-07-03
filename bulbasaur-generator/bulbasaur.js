d9b7eb9c4d808d827799fc878877e83cbdc9dc34
Kai <something.kai@gmail.com>
2019-07-03 13:48:48 -0700

    working

--- bulbasaur-generator/bulbasaur.js
+++ bulbasaur-generator/bulbasaur.js
@@ -117,27 +117,21 @@ async function createBulbasaur(){
       return (document.getElementById("pkmn").innerHTML = svg);
 }
 
+// darkens or lightens a hex color by given amt 
 function LightenDarkenColor(col,amt) {
     var usePound = false;
     if ( col[0] == "#" ) {
         col = col.slice(1);
         usePound = true;
     }
-
     var num = parseInt(col,16);
-
     var r = (num >> 16) + amt;
-
     if ( r > 255 ) r = 255;
     else if  (r < 0) r = 0;
-
     var b = ((num >> 8) & 0x00FF) + amt;
-
     if ( b > 255 ) b = 255;
     else if  (b < 0) b = 0;
-
     var g = (num & 0x0000FF) + amt;
-
     if ( g > 255 ) g = 255;
     else if  ( g < 0 ) g = 0;
 
